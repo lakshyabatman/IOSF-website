2bb1ea293e238a84a9c32094d91fd0d7b9acc1e8
sbabureddy <sbabureddy01@gmail.com>
2020-09-01 21:02:15 +0530

    Fix Button Color and Spacing

--- src/components/Home/components/Footer/AppFooter.js
+++ src/components/Home/components/Footer/AppFooter.js
@@ -43,7 +43,7 @@ class AppFooter extends React.Component {
               </div>
               <div className="column has-text-centered">
                 <Button
-                  className="footer__joinUsBtn"
+                  className="footer__joinUsBtn button__join"
                   size="medium"
                   renderAs="a"
                   href="https://toolsiosf.typeform.com/to/fl45kmgs"
@@ -111,7 +111,7 @@ class AppFooter extends React.Component {
                       </div>
                     </div>
                     <Button
-                      className="footer__subscribeBtn"
+                      className="footer__subscribeBtn "
                       color="primary"
                       size="small"
                     >
@@ -133,57 +133,57 @@ class AppFooter extends React.Component {
               </Columns>
             </div>
             <hr />
-            {/* <div className="footer__copyright"> */}
-            <Level>
-              <Level.Side align="left">
-                <Level.Item>
-                  <p>
-                    ping@iosf.com <br />
-                    &copy; Indian Open Source Foundation, {date.getFullYear()}
-                  </p>
-                </Level.Item>
-              </Level.Side>
-              <Level.Side align="right">
-                <Level.Item>
-                  <Columns.Column size={12}>
-                    <h4>Connect</h4>
-                    <div className="footer__socialLinks">
-                      <a
-                        href="https://www.facebook.com/iosfindia/"
-                        className="btn btn-floating z-depth-0 facebook transparent grey-text"
-                      >
-                        <i className="fab grey-text fa-facebook-f"></i>
-                      </a>
-                      <a
-                        href="https://twitter.com/iosf_india"
-                        className="btn btn-floating z-depth-0 twitter transparent grey-text"
-                      >
-                        <i className="fab grey-text fa-twitter"></i>
-                      </a>
-                      <a
-                        href="https://www.instagram.com/iosf_india/"
-                        className="btn btn-floating z-depth-0 instagram transparent grey-text"
-                      >
-                        <i className="fab grey-text fa-instagram"></i>
-                      </a>
-                      <a
-                        href="https://www.linkedin.com/company/iosf/"
-                        className="btn btn-floating z-depth-0 linkedin transparent grey-text"
-                      >
-                        <i className="fab grey-text fa-linkedin"></i>
-                      </a>
-                      <a
-                        href="https://github.com/IndianOpenSourceFoundation"
-                        className="btn btn-floating z-depth-0 github transparent grey-text"
-                      >
-                        <i className="fab grey-text fa-github"></i>
-                      </a>
-                    </div>
-                  </Columns.Column>
-                </Level.Item>
-              </Level.Side>
-            </Level>
-            {/* </div> */}
+            <div className="footer__copyright">
+              <Level>
+                <Level.Side align="left">
+                  <Level.Item className="justify__text__left">
+                    <p>
+                      ping@iosf.com <br />
+                      &copy; Indian Open Source Foundation, {date.getFullYear()}
+                    </p>
+                  </Level.Item>
+                </Level.Side>
+                <Level.Side align="right">
+                  <Level.Item>
+                    <Columns.Column size={12}>
+                      <h4>Connect</h4>
+                      <div className="footer__socialLinks">
+                        <a
+                          href="https://www.facebook.com/iosfindia/"
+                          className="btn btn-floating z-depth-0 facebook transparent grey-text"
+                        >
+                          <i className="fab grey-text fa-facebook-f"></i>
+                        </a>
+                        <a
+                          href="https://twitter.com/iosf_india"
+                          className="btn btn-floating z-depth-0 twitter transparent grey-text"
+                        >
+                          <i className="fab grey-text fa-twitter"></i>
+                        </a>
+                        <a
+                          href="https://www.instagram.com/iosf_india/"
+                          className="btn btn-floating z-depth-0 instagram transparent grey-text"
+                        >
+                          <i className="fab grey-text fa-instagram"></i>
+                        </a>
+                        <a
+                          href="https://www.linkedin.com/company/iosf/"
+                          className="btn btn-floating z-depth-0 linkedin transparent grey-text"
+                        >
+                          <i className="fab grey-text fa-linkedin"></i>
+                        </a>
+                        <a
+                          href="https://github.com/IndianOpenSourceFoundation"
+                          className="btn btn-floating z-depth-0 github transparent grey-text"
+                        >
+                          <i className="fab grey-text fa-github"></i>
+                        </a>
+                      </div>
+                    </Columns.Column>
+                  </Level.Item>
+                </Level.Side>
+              </Level>
+            </div>
           </Content>
         </Container>
       </Footer>
